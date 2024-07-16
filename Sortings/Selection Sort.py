2f5aa1646dd24d311f6a069116ca07a08f1a04d5
Arsh Sharma <105605204+Arsh901@users.noreply.github.com>
2024-07-16 13:57:49 +0530

    Add files via upload

---
+++ Sortings/Selection Sort.py
@@ -0,0 +1,26 @@
+# Selection Sort
+"""We repeatedly find the minimum element and move it to sorted part of array to make unsorted part sorted"""
+
+"""
+When to use this sorting -
+  --> When size of the list is small. 
+  --> No extra space is needed
+When to avoid - 
+  --> The size of list is very large. Finding min element again and again is difficult
+"""
+
+def selection_sort(customList):
+    for i in range(len(customList)):
+        min_index = i
+        for j in range(i+1, len(customList)):
+            if customList[min_index]>customList[j]:
+                min_index = j
+        customList[i], customList[min_index] = customList[min_index], customList[i]
+    print(customList)
+
+selection_sort([9, 11, 4, 20, 15, 1, 22, 14])
+
+
+
+
+
